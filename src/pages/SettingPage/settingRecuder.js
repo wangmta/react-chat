72a5c6a889a0bf67336264217589933cfa06d8b4
wangmta <wangmta@gmail.com>
2020-03-27 12:53:21 -0400

    feat: add settingReducer

--- src/pages/SettingPage/settingRecuder.js
+++ src/pages/SettingPage/settingRecuder.js
@@ -1,5 +1,30 @@
+import { SET_GLOBAL_SETTINGS } from './settingAction';
+
 const GLOBAL_SETTINGS = {
   NOTIFICATION: 'notification'
 };
 
-export { GLOBAL_SETTINGS };
+const initialSettings = {
+  [GLOBAL_SETTINGS.NOTIFICATION]: true
+};
+
+const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+const previusSettings =
+  userInfo && JSON.parse(localStorage.getItem(`settings-${userInfo.user_id}`));
+
+const setGlobalSettingsReducer = (previousState = previusSettings || initialSettings, action) => {
+  switch (action.type) {
+    case SET_GLOBAL_SETTINGS:
+      if (userInfo) {
+        localStorage.setItem(
+          `settings-${userInfo.user_id}`,
+          JSON.stringify({ ...previousState, ...action.data })
+        );
+      }
+      return { ...previousState, ...action.data };
+    default:
+      return previousState;
+  }
+};
+
+export { GLOBAL_SETTINGS, setGlobalSettingsReducer };
