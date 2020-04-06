c27277771bc4344357bec261e1ce87f344be0126
wangmta <wangmta@gmail.com>
2020-04-06 16:47:33 -0400

    style: add chat header style

--- src/components/ChatHeader/index.js
+++ src/components/ChatHeader/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import './style.scss';
 
 class ChatHeader extends React.Component {
   goBack = () => {
